7b75189acddce8b54b4f038b696430425bd91f0a
Adam Baxter <voltagex@voltagex.org>
2018-10-29 01:42:20 +1100

    Force module install

--- helper.ps1
+++ helper.ps1
@@ -1,9 +1,8 @@
 choco install -y msys2 7zip.install
-7z
 
 C:\tools\msys64\usr\bin\bash.exe -lc "pacman --noconfirm -S python mingw-w64-x86_64-toolchain gettext python3 mingw-w64-x86_64-glib2 mingw-w64-x86_64-pixman make bison mingw-w64-x86_64-diffutils mingw-w64-cross-binutils mingw-w64-x86_64-SDL2"
 
-Install-Module -Name PS.B2
+Install-Module -Name PS.B2 -Force
 Import-Module -Name PS.B2
 
 Connect-B2Cloud -AccountID $(Secret.B2_KEY_ID) -ApplicationKey $(Secret.B2_APPLICATION_KEY)
