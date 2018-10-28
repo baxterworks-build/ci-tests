86ddeed79ebd89f0888edfcec62ebc2b2c61ff7f
Adam Baxter <voltagex@voltagex.org>
2018-10-29 09:42:48 +1100

    Secret variables are hard

--- helper.ps1
+++ helper.ps1
@@ -1,12 +1,14 @@
-choco install -y msys2 7zip.install
+#choco install -y msys2 7zip.install
 
-C:\tools\msys64\usr\bin\bash.exe -lc "pacman --noconfirm -S python mingw-w64-x86_64-toolchain gettext python3 mingw-w64-x86_64-glib2 mingw-w64-x86_64-pixman make bison mingw-w64-x86_64-diffutils mingw-w64-cross-binutils mingw-w64-x86_64-SDL2"
+#C:\tools\msys64\usr\bin\bash.exe -lc "pacman --noconfirm -S python mingw-w64-x86_64-toolchain gettext python3 mingw-w64-x86_64-glib2 mingw-w64-x86_64-pixman make bison mingw-w64-x86_64-diffutils mingw-w64-cross-binutils mingw-w64-x86_64-SDL2"
 
 Install-Module -Name PS.B2 -Force
 Import-Module -Name PS.B2
 
-Connect-B2Cloud -AccountID $B2_KEY_ID -ApplicationKey $B2_APPLICATION_KEY
+Connect-B2Cloud -AccountID $env:B2_KEY_ID -ApplicationKey $env:B2_APPLICATION_KEY
 $bucketId = (Get-B2Bucket | Where-Object -Property BucketName -eq "baxterworks-azure").BucketID
-7z a msys2_tools.7z c:\tools
+Write-Host $bucketId
 
-Invoke-B2ItemUpload -BucketID $bucketId -Path msys2_tools.7z
+#7z a msys2_tools.7z c:\tools
+
+#Invoke-B2ItemUpload -BucketID $bucketId -Path msys2_tools.7z
