f5defaaef1d57bf28af07c175ced8e40e8b35ee3
Adam Baxter <voltagex@voltagex.org>
2018-10-30 00:13:40 +1100

    Switch to vs2017 image and download from b2

--- helper.ps1
+++ helper.ps1
@@ -1,11 +1,23 @@
-choco install -y msys2 7zip.install
-C:\tools\msys64\usr\bin\bash.exe -lc "pacman --noconfirm -S python mingw-w64-x86_64-toolchain gettext python3 mingw-w64-x86_64-glib2 mingw-w64-x86_64-pixman make bison mingw-w64-x86_64-diffutils mingw-w64-cross-binutils mingw-w64-x86_64-SDL2"
+# choco install -y msys2 7zip.install
+# C:\tools\msys64\usr\bin\bash.exe -lc "pacman --noconfirm -S python mingw-w64-x86_64-toolchain gettext python3 mingw-w64-x86_64-glib2 mingw-w64-x86_64-pixman make bison mingw-w64-x86_64-diffutils mingw-w64-cross-binutils mingw-w64-x86_64-SDL2"
 
-Install-Module -Name PS.B2 -Force
-Import-Module -Name PS.B2
+# Install-Module -Name PS.B2 -Force
+# Import-Module -Name PS.B2
 
-Connect-B2Cloud -AccountID $env:B2_KEY_ID -ApplicationKey $env:B2_APPLICATION_KEY
-$bucketId = (Get-B2Bucket | Where-Object -Property BucketName -eq "baxterworks-azure").BucketID
+# Connect-B2Cloud -AccountID $env:B2_KEY_ID -ApplicationKey $env:B2_APPLICATION_KEY
+# $bucketId = (Get-B2Bucket | Where-Object -Property BucketName -eq "baxterworks-azure").BucketID
 
-7z a msys2_tools.7z c:\tools
-Invoke-B2ItemUpload -BucketID $bucketId -Path msys2_tools.7z
+# 7z a msys2_tools.7z c:\tools
+# Invoke-B2ItemUpload -BucketID $bucketId -Path msys2_tools.7z
+
+
+
+
+choco install -y 7zip.install aria2
+py -m pip install b2
+b2 authorize-account $env:B2_KEY_ID $env:B2_APPLICATION_KEY
+$url = b2 get-download-url-with-auth baxterworks-azure msys2_tools.7z
+mkdir c:\tools; cd c:\tools
+aria2 -x5 -j5 $url
+
+7z x msys2_tools.7z
