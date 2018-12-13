21a08231042f461e7249d89fb4e09d7657e773fc
Adam Baxter <voltagex@voltagex.org>
2018-12-13 20:56:10 +1100

    test script for bash

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin env bash
+
+for c in wget curl aria2c bash gcc sh apt yum pacman readlink git nproc; do
+    echo $c: $(command -v $c || echo no)
+done;
+echo
+
+command -v nproc && echo CPUs: $(nproc)
