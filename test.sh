4abbb0f2616cfedede0d91fb5d9fdd365b252fe2
Adam Baxter <voltagex@voltagex.org>
2019-01-28 17:42:17 +1100

    Update test.sh

--- test.sh
+++ test.sh
@@ -16,15 +16,5 @@ echo "apt update for traceroute"
 apt update &> /dev/null
 apt -y install traceroute iproute2 &> /dev/null
 
+echo
 traceroute 8.8.8.8
-
-
-echo "Console slowdown test:"
-
-echo "With output"
-time (for i in {1..1000}; do echo console output $i; done)
-
-echo "No output"
-time (for i in {1..1000}; do echo console output $i; done) >/dev/null
-
-
