b93c50bc9c225ef25f58c3e2ff38014ab5f40c16
Adam Baxter <voltagex@voltagex.org>
2020-01-05 16:49:23 +1100

    Test exiting a pipeline from a script

--- console-slowdown.sh
+++
@@ -1,7 +0,0 @@
-echo "Console slowdown test:"
-
-echo "With output"
-time (for i in {1..1000}; do echo console output $i; done)
-
-echo "No output"
-time (for i in {1..1000}; do echo console output $i; done) >/dev/null
