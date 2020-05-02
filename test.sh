6bd4c2e994a04bf447b9de330c64e2561053f7df
Adam Baxter <voltagex@voltagex.org>
2020-05-02 23:03:58 +1000

    Unfuck test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,26 @@
 #!/usr/bin env bash
 
+for c in wget curl aria2c bash gcc sh apt yum pacman readlink git nproc; do
+    echo $c: $(command -v $c || echo no)
+done;
+echo
 
+command -v nproc && echo CPUs: $(nproc)
+free -m
+
+echo whoami? $(whoami)
+sudo -nv || echo sudo unavailable
 echo
 echo "Current path is $PWD"
 echo "Environment:"
 export
 
-exit 78
+echo "apt update for traceroute"
+apt update || echo apt update unavailable
+apt -y install traceroute iproute2 || echo apt install unavailable
 
-<<<<<<< HEAD
-=======
 echo
 echo
 traceroute 8.8.8.8 || echo traceroute not found
 
-
 docker --version || echo docker unavailable
->>>>>>> Test for docker
