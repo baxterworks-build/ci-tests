7325c054b9bc97e1abf5a289ce368f39faa9ad27
Adam Baxter <voltagex@voltagex.org>
2020-05-02 23:00:34 +1000

    Revert "Test exiting a pipeline from a script"
    This reverts commit b93c50bc9c225ef25f58c3e2ff38014ab5f40c16.

--- test.sh
+++ test.sh
@@ -1,10 +1,24 @@
 #!/usr/bin env bash
 
+for c in wget curl aria2c bash gcc sh apt yum pacman readlink git nproc; do
+    echo $c: $(command -v $c || echo no)
+done;
+echo
 
+command -v nproc && echo CPUs: $(nproc)
+free -m
+
+echo whoami? $(whoami)
+sudo -nv || echo sudo unavailable
 echo
 echo "Current path is $PWD"
 echo "Environment:"
 export
 
-exit 78
+echo "apt update for traceroute"
+apt update || echo apt update unavailable
+apt -y install traceroute iproute2 || echo apt install unavailable
 
+echo
+echo
+traceroute 8.8.8.8 || echo traceroute not found
