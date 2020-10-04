96cb6500ff46cce39904653e6fb2256e3af31a07
Adam Baxter <voltagex@voltagex.org>
2020-10-04 21:09:00 +1100

    packet.net focused mirrors

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/usr/bin env bash
 
+function mtr_report() {
+    echo $1
+    mtr -r -c 1 $1
+    echo
+}
+
 for c in wget curl aria2c bash gcc sh apt yum pacman readlink git nproc; do
     echo $c: $(command -v $c || echo no)
 done;
@@ -19,11 +25,10 @@ if [ -f "/etc/debian_version" ]; then
     echo "apt update for mtr"
     apt update || echo apt update unavailable
     apt -y install mtr-tiny || echo apt install unavailable
-    echo deb.debian.org
-    mtr -r -c 1 deb.debian.org
-    echo
-    echo mirror.rit.edu
-    mtr -r -c 1 mirror.rit.edu
+    mtr_report deb.debian.org
+    mtr_report mirror.rit.edu
+    mtr_report gnu.mirror.constant.com
+    mtr_report gnu.spinellicreations.com
 
 else
     traceroute 8.8.8.8 || echo traceroute not found
