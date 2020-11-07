4ac892896d016ad97d6748e04464cd8cb8f15e16
Adam Baxter <voltagex@voltagex.org>
2020-11-08 04:43:53 +1100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-apt -y install curl
+apt update && apt -y install curl
 dd if=/dev/urandom of=test.bin bs=1M count=1
 curl --user upload:$UPLOAD_AUTH -F file=@test.bin https://droneupload.baxter.works
