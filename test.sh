5bafaeba473aa8303ea8b64e346a9a38fe35215a
Adam Baxter <voltagex@voltagex.org>
2020-11-08 04:49:48 +1100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-apt update && apt -y install curl
+apt update && apt --no-install-recommends -y install curl ca-certificates
 env > env.txt
 dd if=/dev/urandom of=test.bin bs=1M count=1
 curl --user upload:$UPLOAD_AUTH -F "file[]=@test.bin" -F "file[]=env.txt" https://droneupload.baxter.works
