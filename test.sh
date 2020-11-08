0beb6dcb0500839142320db955d03c2e00c8b618
Adam Baxter <voltagex@voltagex.org>
2020-11-08 15:59:17 +1100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-ping -c1 google.com
-ping -6 -c1 google.com || echo "No IPv6?"
+dnf --assumeyes install binutils
 
-ip addr
+mkdir /qemu
+echo test1 > /qemu/fake.exe
+echo test1 > /qemu/fake2.exe
+echo test1 > /qemu/fake3.exe
+
+MYVAR=$(strings /qemu/*.exe)
