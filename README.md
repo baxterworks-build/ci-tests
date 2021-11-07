77b438882dd11053939d01102530985ac0752384
Adam Baxter <git@voltagex.org>
2021-11-07 17:38:25 +1100

    Cleanup

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# azure-pipelines-tests
-
-Creating a readme because Azure Pipelines can't cope without an initial commit
+# CI Tests
+Noodling around with some network tests to run on CI platforms.
+Generally a junk repo and should be ignored.
--- test.sh
+++ test.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-ping -c1 1.1.1.1
-ping -6 -c1 2606:4700:4700::1111 || echo "No IPv6?"
-ip addr
+set -eou pipefail
 
-sudo apt -y update && sudo apt -y install mtr-tiny
 HOSTS="github.com gitlab.com dev.azure.com 1.1.1.1 8.8.8.8 deb.debian.org mirrors.kernel.org mirrorservice.org"
-for h in $HOSTS; do mtr --report-wide -c3 -e -z $h; echo; done
+for h in $HOSTS; do echo $h; mtr --report-wide -c2 -e -z $h; echo; done
 echo
-export
-
