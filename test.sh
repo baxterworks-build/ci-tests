ab6ee538ddeccecffa21e2d5fd47304f2634ffa5
Adam Baxter <git@voltagex.org>
2021-11-07 17:25:20 +1100

    Can we install packages on the fly?

--- test.sh
+++ test.sh
@@ -3,11 +3,9 @@ ping -c1 1.1.1.1
 ping -6 -c1 2606:4700:4700::1111 || echo "No IPv6?"
 ip addr
 
-apt -y update && apt -y install mtr-tiny
+sudo apt -y update && sudo apt -y install mtr-tiny
 HOSTS="github.com gitlab.com dev.azure.com 1.1.1.1 8.8.8.8 deb.debian.org mirrors.kernel.org mirrorservice.org"
 for h in $HOSTS; do mtr --report-wide -c3 -e -z $h; echo; done
 echo
 export
-
-cat /proc/cpuinfo
 
